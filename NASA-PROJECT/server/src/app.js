7c75bf7d7eefacdc1e86a3f0108eba09928bb669
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-08-07 19:27:39 -0400

    CORS middleware added.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

--- NASA-PROJECT/server/src/app.js
+++ NASA-PROJECT/server/src/app.js
@@ -1,11 +1,17 @@
 //set up express
 const express = require('express');
+//import cors (allow-cross-origin)
+const cors = require('cors');
 
 //import
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
+app.use(cors({
+  //except requests only frpm local host
+  origin: 'http://localhost:3000',
+})); 
 //check coming data for json content
 app.use(express.json());
 app.use(planetsRouter);
