7b9e1abb1965e29dfce0bed9c5787dfab9381424
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-08-09 23:23:02 -0400

    Serving Reactjs front end in production.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

--- NASA-PROJECT/server/src/app.js
+++ NASA-PROJECT/server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 //set up express
 const express = require('express');
 //import cors (allow-cross-origin)
@@ -5,6 +6,7 @@ const cors = require('cors');
 
 //import
 const planetsRouter = require('./routes/planets/planets.router');
+const e = require('express');
 
 const app = express();
 
@@ -12,8 +14,15 @@ app.use(cors({
   //except requests only frpm local host
   origin: 'http://localhost:3000',
 })); 
-//check coming data for json content
-app.use(express.json());
+
+//express middleware
+app.use(express.json());//check coming data for json content  
+app.use(express.static(path.join(__dirname, '..', 'public'))); //get path to public files
+
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+  //send index.html inresponse
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 module.exports = app;
