b4ee7eaea00a106ebb3ac1b5ae075daddef6d703
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-08-09 23:56:28 -0400

    Set BUILD_PATH, logging requests with morgan middleware.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

--- NASA-PROJECT/server/src/app.js
+++ NASA-PROJECT/server/src/app.js
@@ -1,8 +1,9 @@
 const path = require('path');
 //set up express
 const express = require('express');
-//import cors (allow-cross-origin)
+//import cors (allow-cross-origin request)
 const cors = require('cors');
+const morgan = require('morgan');
 
 //import
 const planetsRouter = require('./routes/planets/planets.router');
@@ -10,15 +11,18 @@ const e = require('express');
 
 const app = express();
 
+//security related feature
 app.use(cors({
   //except requests only frpm local host
   origin: 'http://localhost:3000',
 })); 
+app.use(morgan('combined'));
 
 //express middleware
 app.use(express.json());//check coming data for json content  
 app.use(express.static(path.join(__dirname, '..', 'public'))); //get path to public files
 
+//api requests
 app.use(planetsRouter);
 app.get('/', (req, res) => {
   //send index.html inresponse
