247da3971e1d8bcdd8dc4b3b704b09787fb9f911
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-08-14 00:33:02 -0400

    DELETE /launches: aborting launches.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

--- NASA-PROJECT/server/src/models/launches.model.js
+++ NASA-PROJECT/server/src/models/launches.model.js
@@ -11,7 +11,7 @@ const launch = {
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'), //javascript date object
-  destination: 'Kepler-442 b',
+  target: 'Kepler-442 b',
   customer: ['ZTM', 'NASA'],
   upcoming: true,
   success: true,
@@ -20,6 +20,9 @@ const launch = {
 //add launches by key, value
 launches.set(launch.flightNumber, launch);
 
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
 
 function getAllLaunches() {
   return Array.from(launches.values()); //return launches converted to an array
@@ -40,7 +43,16 @@ function addNewLaunch(launch) {
     ); 
 };
 
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId); //get data
+  aborted.upcoming = false; //data will be send to history list
+  aborted.success = false;
+  return aborted;
+}
+
 module.exports = {
+  existsLaunchWithId,
   getAllLaunches,
   addNewLaunch,
+  abortLaunchById,
 }
--- NASA-PROJECT/server/src/routes/launches/launches.router.js
+++ NASA-PROJECT/server/src/routes/launches/launches.router.js
@@ -3,11 +3,13 @@ const express = require('express');
 const { 
   httpGetAllLaunches,
   httpAddNewLaunch,
+  httpAbortLaunch,
 } = require('./launches.controller');
 const launchesRouter = express.Router();
 
 //define routes
 launchesRouter.get('/', httpGetAllLaunches);
 launchesRouter.post('/', httpAddNewLaunch);
+launchesRouter.delete('/:id', httpAbortLaunch); //passing id parameter
 
 module.exports = launchesRouter;
