7c80f191619e850e7d49b8c65f4aeaa48c066d16
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-08-13 23:56:38 -0400

    POST /launches connected with front end dashboard.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

--- NASA-PROJECT/client/src/hooks/requests.js
+++ NASA-PROJECT/client/src/hooks/requests.js
@@ -16,8 +16,20 @@ async function httpGetLaunches() {
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
   // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch), //convert object into string to pass directly into API
+    });
+  } catch(err) {
+    return {
+      ok: false, //success response is false
+    };
+  }
 }
 
 async function httpAbortLaunch(id) {
