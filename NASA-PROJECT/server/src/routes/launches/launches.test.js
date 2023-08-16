c84ef2eec7804da27be68ac626525cb3577abfdd
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-08-16 00:32:08 -0400

    #1. Testing API with Jest.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

---
+++ NASA-PROJECT/server/src/routes/launches/launches.test.js
@@ -0,0 +1,15 @@
+describe('Test GET /launches', () => {
+  test('It should respond with 200 success', () => {
+    //test code
+    const response = 200;
+    expect(response).toBe(200);
+  });
+});
+
+describe('Test POST /launch', () => {
+ test('It should respond with 200 success', () => {});
+
+ test('It should catch missing required property', () => {}); 
+ 
+ test('It should catch invalid dates', () => {});
+});
