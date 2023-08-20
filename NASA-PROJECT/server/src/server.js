31dacb626086192d4fb8977e43619d24a8cd9112
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-08-20 15:38:10 -0400

    MongoDB Atlas set up, connected to MongoDB.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

--- NASA-PROJECT/server/src/server.js
+++ NASA-PROJECT/server/src/server.js
@@ -1,5 +1,6 @@
 //create built in node http server
 const http = require('http');
+const mongoose = require('mongoose');
 
 //import app 
 const app = require('./app');
@@ -14,9 +15,24 @@ const { loadPlanetsData } = require('./models/planets.model');
 - Check first if there is a port specified in the environment, if it is not, go to the default 8000.
 */
 const PORT = process.env.PORT || 8000; 
+
+const MONGO_URL = 'mongodb+srv://nasa-api:1iQT2FOpYFnF2G5K@nasacluster.nl8wtvg.mongodb.net/nasa?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+//notify that MongoDB is connected
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection is ready!');
+});
+
+//catch errors
+mongoose.connection.on('error', (err) => {
+ console.error(err);
+});
+
 async function startServer() {
+  //connect to the MongoDB using latest features
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
