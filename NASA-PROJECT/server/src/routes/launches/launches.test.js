bcd130a468748031b6953cd6c941171e47fe5448
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-10-26 22:57:40 -0400

    Load planets data in tests.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

--- NASA-PROJECT/server/src/routes/launches/launches.test.js
+++ NASA-PROJECT/server/src/routes/launches/launches.test.js
@@ -6,10 +6,12 @@ const {
   mongoConnect, 
   mongoDisconnect, 
 } = require('../../services/mongo');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Launches API', () => {
   beforeAll(async () => {
     await mongoConnect(); //connect to the database
+    await loadPlanetsData();
   });
 
   afterAll(async () => {
